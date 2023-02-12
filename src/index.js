2dc3c218831d0944478f8610e42effd899ff8667
Anton Belski <anton.belski@gmail.com>
2023-02-12 17:57:01 +0300

    feat:add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let string = str;
+    for (i = string.length; i >= 0; i--) {
+      
+        for (l = 0; l < bracketsConfig.length; l++) {
+            const bracketsPair = bracketsConfig[l].join('');
+            if (string.includes(bracketsPair)) {
+                string = string.replace(bracketsPair, '');
+            }
+           
+        }
+    }
+    return string ? false : true;
 }
